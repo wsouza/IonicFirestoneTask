8a1fb59bbc641e6167c4b4226151541ad712801e
= <=>
2019-10-11 21:01:58 -0300

    login funcionando

---
+++ src/app/auth/pages/login/login.page.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+import { AuthService } from 'src/app/core/services/auth.service';
+import { AuthProvider } from 'src/app/core/services/auth.types';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage implements OnInit {
+  authForm: FormGroup;
+  authProviders = AuthProvider;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Create account'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get name(): FormControl {
+    return  this.authForm.get('name') as FormControl;
+  }
+
+  get email(): FormControl {
+    return  this.authForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return  this.authForm.get('password') as FormControl;
+  }
+
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sign Up';
+    this.configs.actionChange = isSignIn ? 'Create account' : 'Already have an account';
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
+  }
+
+  async onSubmit(provider: AuthProvider): Promise<void> {
+    try {
+      const credentials = await this.authService.authenticate({
+        isSignIn: this.configs.isSignIn,
+        user: this.authForm.value,
+        provider
+      });
+      console.log('Authenticated: ', credentials);
+      console.log('Redirecting...');
+    } catch (e) {
+      console.log('Auth error: ', e);
+    }
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBR3IwYuW-HCXZ_eEmgPJIR1csv1I0K1QA',
+    authDomain: 'ionic-firestore-tasks-curso.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks-curso.firebaseio.com',
+    projectId: 'ionic-firestore-tasks-curso',
+    storageBucket: 'ionic-firestore-tasks-curso.appspot.com',
+    messagingSenderId: '625147182144',
+    appId: '1:625147182144:web:d5ddde57d36da114efb3a5'
+  }
 };
 
 /*
